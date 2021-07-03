cd50fc75cc12a4ad293f9297120b3c2d37b0c4dd
young <mould837@naver.com>
2021-07-03 21:59:10 +0900

    0702 Dao, Vo 향상된 코드

---
+++ src/com/javaex/ex03/AuthorVo.java
@@ -0,0 +1,63 @@
+package com.javaex.ex03;
+
+public class AuthorVo {
+
+	//필드
+	private int authorId;
+	private String authorName;
+	private String authorDesc;
+	
+	//생성자
+	public AuthorVo() {
+		
+	}
+	
+	public AuthorVo(String authorName, String authorDesc) {
+		super();
+		this.authorName = authorName;
+		this.authorDesc = authorDesc;
+	}
+	
+	public AuthorVo(int authorId, String authorName, String authorDesc) {
+		super();
+		this.authorId = authorId;
+		this.authorName = authorName;
+		this.authorDesc = authorDesc;
+	}
+	
+	
+	//메소드 겟터셋터
+	public int getAuthorId() {
+		return authorId;
+	}
+	
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+	
+	public String getAuthorName() {
+		return authorName;
+	}
+	
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	
+	public String getAuthorDesc() {
+		return authorDesc;
+	}
+	
+	public void setAuthorDesc(String authorDesc) {
+		this.authorDesc = authorDesc;
+	}
+
+	
+	//메소드 일반
+	@Override
+	public String toString() {
+		return "작가번호: " + authorId + "\t작가이름: " + authorName + "\t작가설명: " + authorDesc;
+	}
+	
+	
+	
+}
